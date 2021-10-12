c03b9128e37590187d924c7e2d24fd2dba76ea66
田俊雄 <v-tianjunxiong@oppo.com>
2021-10-12 19:20:00 +0800

    1

---
+++ src/solutions/Trie.java
@@ -0,0 +1,34 @@
+package solutions;
+
+public class Trie {
+
+    static class TrieNode {
+        char[] chs;
+    }
+
+
+    public Trie() {
+
+    }
+
+    /**
+     * 向前缀树插入字符串
+     */
+    public void insert(String word) {
+
+    }
+
+    /**
+     * 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 false
+     */
+    public boolean search(String word) {
+
+    }
+
+    /**
+     * 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否则，返回 false
+     */
+    public boolean startsWith(String prefix) {
+
+    }
+}
--- src/solutions/_1233_removeSubfolders.java
+++ src/solutions/_1233_removeSubfolders.java
@@ -12,12 +12,13 @@ class _1233_removeSubfolders {
         final int len = 27;
         final int maxLen = getMaxFolderLength(folderSet);
         final boolean[][] flag = new boolean[maxLen][len];
-        //final boolean[] isTail = new boolean[maxLen - 1];
+        final boolean[][] isTail = new boolean[maxLen][len];
         for (String s : folder) {
             int i = 1;
             boolean isExist = true;
+            char ch = '-';
             for (; i < s.length(); i++) {
-                final char ch = s.charAt(i);
+                ch = s.charAt(i);
                 if ('/' == ch) {
                     if (flag[i - 1][len - 1] && isExist) {
                         folderSet.remove(s);
@@ -29,6 +30,7 @@ class _1233_removeSubfolders {
                 flag[i - 1][ch - 'a'] = true;
             }
             flag[s.length() - 1][len - 1] = true;
+            isTail[s.length() - 1][ch - 'a'] =true;
             //isTail[i - 1] = true;
         }
         return new ArrayList<>(folderSet);
@@ -43,6 +45,11 @@ class _1233_removeSubfolders {
         }
         return len;
     }
+
+    /**
+     * /ad /bd /ca /cd/a
+     * @param args
+     */
 
     public static void main(String[] args) {
         final String[] strings = {"/a", "/a/b", "/c/d", "/c/d/e", "/c/f"};
